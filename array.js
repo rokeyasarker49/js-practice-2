cdeb253f98352b1c29f285970d3f42210da8610c
Rokeya Sarker <133252712+rokeyasarker49@users.noreply.github.com>
2023-06-15 13:10:15 +0600

    Add files via upload

---
+++ array.js
@@ -0,0 +1,104 @@
+var name = "Rokeya Sarker" ;
+//console.log(name)
+
+var nameOfMyFriends = ["Subah Sadaf" , "Syeda Shamima" , "Umme Kulsum"]
+//console.log(nameOfMyFriends)
+
+var oddNumbers = [1,3,5,7,9];
+//console.log(oddNumbers)
+
+var evenNumbers = [2,4,6,8,10];
+//console.log(evenNumbers)
+
+var vowels = ["a", "e", "i", "o", "u"];
+//console.log(vowels.length)
+//console.log(vowels.indexOf("h"));
+
+var myFavouriteChapter = ["C Program", "Html", "CSS", "JavaScript", "Python"];
+//console.log(myFavouriteChapter)
+myFavouriteChapter[3] = "jQuery" ;
+//console.log(myFavouriteChapter)
+indexOfjQuery = myFavouriteChapter.indexOf("jQuery")
+//console.log(indexOfjQuery)
+myFavouriteChapter[indexOfjQuery] = "JavaScript"
+//console.log(myFavouriteChapter)
+
+var friends = ["Syeda Shamima", "Subah Sadaf"];
+//friends.push("Umme Kulsum")
+//console.log(friends)
+//friends.push("Bushra")
+//friends.pop("Umme Kulsum")
+//console.log(friends)
+friends.unshift("Umme Kulsum");
+//console.log(friends)
+friends.shift()
+//console.log(friends)
+
+var moneyOfSubah = 500;
+var moneyOfShuchy = 1000;
+//console.log(moneyOfSubah == moneyOfShuchy)
+//console.log(moneyOfSubah != moneyOfShuchy)
+
+var isShuchyJobHolder = true;
+var isShuchyLooksBeautiful = true;
+var isSheProgrammer = false;
+var isHerSalary = 50000;
+//console.log(isShuchyJobHolder && isShuchyLooksBeautiful && isSheProgrammer && isHerSalary > 60000)
+//console.log(isShuchyLooksBeautiful || isHerSalary <=60000)
+
+var doYouLoveMe = false;
+
+/*if(doYouLoveMe){
+        console.log("I Hate You")
+}
+else {
+        console.log("I Also Love You")
+}*/
+
+var isRaining = true;
+var isNotRaining = false;
+
+/*if(isRaining){
+        console.log("I will go to Ramna Park")
+}
+else if(isNotRaining){
+        console.log("I will go to university")
+}
+else{
+        console.log("I will sleep")
+}*/
+
+var myPocketMoney = 1000;
+var priceOfHalim = 200;
+var priceOfNoodles = 150;
+var priceOfRamen = 400;
+var priceOfBurger = 400;
+var priceOfGrill = 1000;
+var priceOfPizza = 999;
+
+/*if(myPocketMoney > priceOfHalim){
+        console.log("I will go for eat Halim")
+}
+else if(myPocketMoney >= priceOfPizza){
+       if(productSituation == "Bashi"){
+        console.log("I do not want to eat pizza")
+        
+       }
+       else if {
+        console.log("I will eat Pizza")
+       }
+}
+else if (myPocketMoney >= priceOfGrill){
+        console.log("I will gofor eat Grill")
+}
+else if(myPocketMoney > priceOfRamen) {
+        console.log("I will go for eat Ramen")
+}
+else if(myPocketMoney > priceOfBurger){
+        console.log("I will go for eat Burger")
+}
+else{
+        console.log("I will sleep in my home")
+}*/
+
+//console.log(10 <= 7)
